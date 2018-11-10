d377841ae3085b2b58ff768fdcbf04ecaf0b9ab7
yucopowo <yucopowo@gmail.com>
2018-11-10 08:01:58 +0800

    toc

--- packages/vremark-toc/index.js
+++ packages/vremark-toc/index.js
@@ -1,19 +1,43 @@
 var visit = require('unist-util-visit');
-var toc = require('mdast-util-toc');
+var toc = require('./mdast-util-toc');
+var data = require('../unist-util-data');
+
+function create(root) {
+    var result = toc(root);
+    return result.map;
+}
 
 module.exports = function plugin(options = {}) {
 
     return function transformer(root) {
 
-        visit(root, 'linkReference', function (node) {
+        var tocCache = null;
 
-            if(
-                (node.label === 'TOC' || node.label === 'toc')
-                && node.identifier === "toc") {
-                var result = toc(root);
-                Object.assign(node, result.map);
+        visit(root, function (node) {
+            return node.type === 'linkReference'
+                && (node.label === 'TOC' || node.label === 'toc')
+                && node.identifier === "toc";
+        }, function (node) {
+            tocCache = tocCache?tocCache:create(root);
+            if(tocCache) {
+                Object.assign(node, tocCache);
             }
+            else{
+                Object.assign(node, {
+                    type: 'text',
+                    value: ''
+                });
+            }
+        });
 
+        tocCache && visit(root, 'heading', function (node) {
+            if(node.__id__){
+                data(node, {
+                    attrs: {
+                        id: node.__id__
+                    }
+                });
+            }
         });
 
         return root;
